53df38da7ec2792dcf41bb28d52d8c97bff2c9da
Yanlok Li <yli@bol.com>
2022-03-27 00:48:00 +0100

    3-add-meta-elements

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'nl-NL'],
+    defaultLocale: 'en-US',
+    localeDetection: false,
+  },
 }
 
 module.exports = nextConfig
